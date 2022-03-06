b0e8d83e9d92aebba307e4c8860dc1061923feeb
yushi0620honda <japan20jun1996@gmail.com>
2022-03-07 01:33:19 +0900

    8章8.2まで完了

---
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.demo.login.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.login.domain.service.UserService;
+
+@Controller
+public class HomeController {
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("/home")
+	public String getHome(Model model) {
+		model.addAttribute("contents", "login/home :: home_contents");
+		
+		return "login/homeLayout";
+	}
+	
+	@PostMapping("/logout")
+	public String postLogput() {
+		
+		return "redirect:/login";
+	}
+}
---
+++ src/main/java/com/example/demo/login/domain/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.demo.login.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.login.domain.repository.UserDao;
+
+@Service
+public class UserService {
+	@Autowired
+	UserDao dao;
+}
