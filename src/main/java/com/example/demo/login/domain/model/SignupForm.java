f6ea88952751c94f77f9acb5f911ac6453761a0d
yushi0620honda <japan20jun1996@gmail.com>
2022-03-12 13:30:42 +0900

    出来ました

--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -14,26 +14,26 @@ import lombok.Data;
 
 @Data
 public class SignupForm {
-	@NotBlank(message = "{require_check}", groups = ValidGroup1.class)
-	@Email(message = "{email_check}", groups = ValidGroup2.class)
+	@NotBlank(groups = ValidGroup1.class)
+	@Email(groups = ValidGroup2.class)
 	private String userId;
 	
-	@NotBlank(message = "{require_check}", groups = ValidGroup1.class)
-	@Length(min = 4, max = 100, message = "{length_check}",  groups = ValidGroup2.class)
-	@Pattern(regexp = "^[a-zA-Z0-9]+$", message = "{pattern_check}",  groups = ValidGroup3.class)
+	@NotBlank(groups = ValidGroup1.class)
+	@Length(min = 4, max = 100,groups = ValidGroup2.class)
+	@Pattern(regexp = "^[a-zA-Z0-9]+$",groups = ValidGroup3.class)
 	private String password;
 	
-	@NotBlank(message = "{require_check}", groups = ValidGroup1.class)
+	@NotBlank(groups = ValidGroup1.class)
 	private String userName;
 	
-	@NotNull(message = "{require_check}", groups = ValidGroup1.class)
+	@NotNull(groups = ValidGroup1.class)
 	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	private Date birthday;
 	
-	@Min(value = 20, message = "{min_check}", groups = ValidGroup2.class)
-	@Max(value = 100, message = "{max_check}", groups = ValidGroup2.class)
+	@Min(value = 20, groups = ValidGroup2.class)
+	@Max(value = 100, groups = ValidGroup2.class)
 	private int age;
 	
-	@AssertFalse(message = "{false_check}", groups = ValidGroup2.class)
+	@AssertFalse(groups = ValidGroup2.class)
 	private boolean marriage;
 }
