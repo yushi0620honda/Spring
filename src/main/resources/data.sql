6a9498ca1bbac8a57d14bedae778a18de8efa955
yushi0620honda <japan20jun1996@gmail.com>
2022-03-14 02:03:44 +0900

    9章まで完了

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT INTO employee (employee_id, employee_name, age)
 VALUES(1, '本多 優士', 25);
 
 INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
-VALUES('yanada@xxx.co.jp', 'password', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
+VALUES('yamada@xxx.co.jp', 'password', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
